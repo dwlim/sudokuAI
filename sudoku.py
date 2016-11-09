0203ce887ec30fdfe1a23b5839c4a4a52291a794
dwlim <danwlim@gmail.com>
2016-11-09 13:16:26 -0500

    some broken solution that you can look at later

--- sudoku.py
+++ sudoku.py
@@ -3,10 +3,15 @@ from __future__ import print_function
 #grid = [0] * 81
 size = 9
 grid = [[0 for x in range(size)] for y in range(size)]
-# grid = [9,3,0,6,0,5,8,0,1,5,0,0,0,0,0,0,0,7,0,0,0,0,
-# 	0,8,4,0,3,0,5,8,0,6,3,2,0,0,0,0,3,0,4,0,1,0,0,0,
-# 	0,7,8,1,0,3,6,0,7,0,6,2,0,0,0,0,0,3,0,0,0,0,0,0,
-# 	0,6,8,0,9,4,0,6,0,3,2]
+grid[0] = [9,3,0,6,0,5,8,0,1]
+grid[1] = [5,0,0,0,0,0,0,0,7]
+grid[2] = [0,0,0,0,0,8,4,0,3]
+grid[3] = [0,5,8,0,6,3,2,0,0]
+grid[4] = [0,0,3,0,4,0,1,0,0]
+grid[5] = [0,0,7,8,1,0,3,6,0]
+grid[6] = [7,0,6,2,0,0,0,0,0]
+grid[7] = [3,0,0,0,0,0,0,0,6]
+grid[8] = [8,0,9,4,0,6,0,3,2]
 
 #prints out the grid
 def printGrid():
@@ -22,14 +27,14 @@ def printGrid():
 #inserts a number at a certain row and column. Example insertNumber(1,1,1)
 #will insert then number 1 in the top left
 def insertNumber(num, row, col):
-	grid[row-1][col-1] = num
+	grid[row][col] = num
 	return
 
 #will check if a certain row is valid (ie. does it contain 1-9)
 def validRow(row):
 	valid = [False] * 9
 	for index in range(len(grid[0])):
-		valid[grid[row-1][index]-1] = True
+		valid[grid[row][index]-1] = True
 	for index in range(len(valid)):
 		if valid[index] == False:
 			return False
@@ -39,7 +44,7 @@ def validRow(row):
 def validCol(col):
 	valid = [False] * 9
 	for index in range(len(grid)):
-		valid[grid[index][col-1]-1] = True
+		valid[grid[index][col]-1] = True
 	for index in range(len(valid)):
 		if valid[index] == False:
 			return False
@@ -52,22 +57,59 @@ def validBox(boxR, boxC):
 	valid = [False] * 9
 	for i in range(len(grid)/3):
 		for j in range(len(grid[0])/3):
-			valid[grid[boxR+i-1][boxC+j-1]-1] = True
+			valid[grid[boxR+i][boxC+j]-1] = True
 	for index in range(len(valid)):
 		if valid[index] == False:
 			return False
 	return True
 
+def getValidNumbers(row, col):
+	if(grid[row][col] == 0):
+		valid = [True] * 9
+		for index in range(len(grid[0])):
+			valid[grid[row][index]-1] = False
+		for index in range(len(grid)):
+			valid[grid[index][col]-1] = False
+		for i in range(len(grid)/3):
+			for j in range(len(grid[0])/3):
+				valid[grid[(row/3 - 1)+i][(col/3 - 1)+j]-1] = False
+		return valid
+	return None
+
+def getAnswer(row, col):
+	v = getValidNumbers(row, col)
+	p = 0
+	ans = 0
+	if(v == None):
+		return grid[row][col]
+	for index in range(len(v)):
+		if(v[index]):
+			p += 1
+			ans = index+1
+	if (p==1):
+		return ans
+	return 0
+
+def fillNumbers():
+	for i in range(len(grid)):
+		for j in range(len(grid[0])):
+			grid[i][j] = getAnswer(i,j) 
+
 #just testing and adding some random numbers and printing grid
-insertNumber(1, 1, 1)
-insertNumber(2, 2, 1)
-insertNumber(3, 3, 1)
-insertNumber(4, 1, 2)
-insertNumber(5, 2, 2)
-insertNumber(6, 3, 2)
-insertNumber(7, 1, 3)
-insertNumber(8, 2, 3)
-insertNumber(9, 3, 3)
+# insertNumber(1, 1, 1)
+# insertNumber(2, 2, 1)
+# insertNumber(3, 3, 1)
+# insertNumber(4, 1, 2)
+# insertNumber(5, 2, 2)
+# insertNumber(6, 3, 2)
+# insertNumber(7, 1, 3)
+# insertNumber(8, 2, 3)
+# insertNumber(9, 3, 3)
 
 printGrid()
-print (validBox(1,1), end="\n")
+# v = getValidNumbers(0,2)
+# print(v, end="\n")
+# print(getAnswer(0,2), end="\n")
+print("---------------------", end="\n\n")
+fillNumbers()
+printGrid()
